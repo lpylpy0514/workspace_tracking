bd4fccc4369be3b5bdb3808b78853c4120bf7d19
lpylpy0514 <1872918507@qq.com>
2023-09-19 14:02:07 +0800

    add experiment for 256 and onnx export for opencv

--- tracking/onnxsimplify.py
+++ tracking/onnxsimplify.py
@@ -5,4 +5,6 @@ model = onnx.load("vttrack.onnx")
 
 model_simp, check = simplify(model)
 
-onnx.save(model_simp, "simplified.onnx")
+onnx.save(model_simp, "object_tracking_vittrack_2023sep.onnx")
+
+# onnx.save(onnx.shape_inference.infer_shapes(onnx.load("vttrack.onnx")), "vttrack.onnx")
