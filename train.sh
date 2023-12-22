876bed0b7ffbb9937c4af8794a1f89ec7b72be35
lpylpy0514 <1872918507@qq.com>
2023-12-22 17:51:07 +0800

    jittered past annotation

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ pip install thop
 cd /18353470163/lpy/workspace_tracking
 # about your tracker
 script="ostrack"
-config="drawtwo"
+config="vitb_jittered_search_embedding"
 num_gpus=2
 num_thread=8
 
