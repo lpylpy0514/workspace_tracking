aa0a4bde4d1d08e18ae162ee83f692640465156c
lpylpy0514 <1872918507@qq.com>
2024-01-04 16:35:15 +0800

    update train 2

--- train.sh
+++ train.sh
@@ -6,6 +6,14 @@ pip install thop
 cd /18353470163/lpy/workspace_tracking
 # about your tracker
 script="ostrack"
+config="vitb_td_osckpt"
+num_gpus=2
+num_thread=8
+
+# training
+python tracking/train.py --script $script --config $config --save_dir ./output --mode multiple --nproc_per_node $num_gpus --use_wandb 0
+
+script="ostrack"
 config="vitb_talpha_osckpt"
 num_gpus=2
 num_thread=8
